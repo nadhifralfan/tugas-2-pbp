ce4423a170efc671a042af539fa10bc4f1ee3e23
nadhifralfan <nadhif.rahman11@ui.ac.id>
2022-09-12 20:02:28 +0700

    Update repo for tugas 2 PBP

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+context = {
+    'item_list' : CatalogItem.objects.all(),
+    'nama' : 'Nadhif Rahman Alfan',
+    'student_id' : '2106751783',
+}
